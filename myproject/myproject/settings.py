be727225f9ac8dd066329b69349c04ec390c02f2
Pravesh <sharmapravesh000@gmail.com>
2024-08-17 18:13:41 +0530

    feat: deployed

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
